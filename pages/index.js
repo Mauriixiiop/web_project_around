78a64533937cdb8b4c435a6752796a0506f0b4f8
Mauricio <maupizarro19@gmail.com>
2025-03-02 20:04:10 -0300

    update pre entrega

--- pages/index.js
+++ pages/index.js
@@ -8,27 +8,26 @@ let profileName = document.querySelector(".profile__name");
 let profileJob = document.querySelector(".profile__job");
 let buttonSubmit = document.querySelector(".popup__submit-button");
 
-function editProfile() {
+function openPopupProfile() {
   handlePopupOpen();
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
 function handlePopupOpen() {
-  openPopup.classList.add("popup_open");
+  openPopup.classList.add("popup_opened");
 }
 
 function handlePopupClose() {
-  openPopup.classList.remove("popup_open");
+  openPopup.classList.remove("popup_opened");
 }
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  openPopup.classList.remove("popup_open");
+  openPopup.classList.remove("popup_opened");
 }
 
 buttonPopup.addEventListener("click", handlePopupOpen);
-buttonPopup.addEventListener("click", editProfile);
-buttonPopup.addEventListener("click", handlePopupClose);
+buttonClosePopup.addEventListener("click", handlePopupClose);
