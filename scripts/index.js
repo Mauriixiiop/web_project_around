ff6e0f6c756765496356e8c95265816bd683d436
Mauricio <maupizarro19@gmail.com>
2025-03-13 22:53:25 -0300

    agrega add js

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,50 @@ const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__job");
 const buttonSubmit = document.querySelector(".popup__submit-button");
 
+const openPopupAdd = document.querySelector(".popup__add");
+const formAdd = document.querySelector(".popup__add-form");
+const addName = document.querySelector(".popup__add-form-input-name");
+const addImage = document.querySelector(".popup__add-form-input-image");
+const buttonSubmitAdd = document.querySelector(".popup__add-submit-button");
+const buttonCloseAdd = document.querySelector(".popup__add-button-close");
+
+const openPopupImage = document.querySelector(".popup__image");
+const closePopupImage = document.querySelector(".popup__image-button-close");
+
+const buttonAddProfile = document.querySelector(".profile__add-button");
+
+const gallery = document.querySelector(".gallery")
+const galleryTemplate = document.querySelector(".gallery__template");
+
+
+
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+
 function openPopupProfile() {
   handlePopupOpen();
   nameInput.value = profileName.textContent;
@@ -15,12 +59,63 @@ function openPopupProfile() {
 }
 
 function handlePopupOpen() {
+  /* Abrir el popup*/
   openPopup.classList.add("popup_opened");
 }
 
 function handlePopupClose() {
+  /* Cerrar el popup*/
   openPopup.classList.remove("popup_opened");
 }
+
+function handlePopupAddOpen () { /*Abrir el otro popup*/
+  openPopupAdd.classList.add("popup__add_opened");
+}
+
+function handlePopupAddClose () { /*Cerrar el otro popup*/
+  openPopupAdd.classList.remove("popup__add_opened");
+}
+
+function addCards() {
+  initialCards.forEach((item) => {
+    const card = createCard(item.name, item.link);
+    gallery.append(card);
+    openPopupAdd.classList.remove("popup__add_opened")
+  });
+}
+
+
+function createCard(name, link) {  /*crea las cartas*/
+  const templateGallery = document.querySelector(#template).content;
+  const card = document.querySelector(".gallery__card").cloneNode(true); /*opción de las cartas*/
+  const cardImage = document.querySelector(".gallery__card-image"); /*selecciona la img*/
+  const cardName = document.querySelector(".gallery__card-name");
+  const cardLike = document.querySelector(".gallery__card-icon");
+  const cardDelete = document.querySelector(".gallery__card-delete");
+
+  cardImage.src = link;
+  cardImage.alt = name;
+  cardName.textContent = name;
+
+  cardDelete.addEventListener("click", () => {
+    card.remove();
+  });
+  cardLike.addEventListener("click", () => {
+    card.classList(".gallery__card-icon-active");
+  });
+  cardImage.addEventListener("click",() => {
+    handlePopupImageOpen(name, link);
+  });
+  return card;
+  }
+
+
+initialCards.forEach(function(card)){
+  const cardElement = createCard(card);
+  const cards = document.querySelector(".gallery");
+  cards.prepend(cardElement);
+}
+
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
@@ -29,7 +124,25 @@ function handleProfileFormSubmit(evt) {
   openPopup.classList.remove("popup_opened");
 }
 
-formElement.addEventListener("submit", handleProfileFormSubmit);
+function handlePopupImageOpen(name, link) {
+  const popupImg = openPopupImage.querySelector(".popup__image");
+  const popupText = openPopupImage.querySelector(".popup__text");
+  popupImg.src = link;
+  popupImg.alt = name;
+  popupText.textContent = name;
+  openPopupImage.classList.add("popup__image_opened");
+}
 
+function handlePopupImageClose() {
+  openPopupImage.classList.remove("popup__image_opened");
+}
+
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
 buttonPopup.addEventListener("click", handlePopupOpen);
 buttonClosePopup.addEventListener("click", handlePopupClose);
+
+buttonAddProfile.addEventListener("click", handlePopupAddOpen);
+buttonCloseAdd.addEventListener("click", handlePopupAddClose);
+formAdd.addEventListener("submit", handlePopupImageOpen);
+closePopupImage.addEventListener("click", handlePopupImageClose);
